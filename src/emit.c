1383893db5a42cd7abd6db778b700735d11d25be
donniebreve <donniebreve@gmail.com>
2021-04-24 16:30:20 -0700

    some kind of strange behavior when setting keybit > 573 (KEY_MAX is 767)

--- src/emit.c
+++ src/emit.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <unistd.h>
 #include <linux/input.h>
 
@@ -9,6 +10,7 @@
 */
 int emit(int type, int code, int value)
 {
+   //printf("emit: code=%i value=%i\n", code, value); 
    struct input_event e;
    e.time.tv_sec = 0;
    e.time.tv_usec = 0;
@@ -19,7 +21,7 @@ int emit(int type, int code, int value)
    write(output, &e, sizeof(e));
    // emit syn event
    e.type = EV_SYN;
-   e.code = 0;
+   e.code = SYN_REPORT;
    e.value = 0;
    write(output, &e, sizeof(e));
    return 0;
