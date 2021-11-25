b3564453f6a588c4d9281e656e88f9bca80d2e3b
donniebreve <donniebreve@protonmail.com>
2021-11-25 12:50:07 -0800

    Added defines for new keys to support LTS distributions (#37)

--- src/keys.h
+++ src/keys.h
@@ -1,6 +1,21 @@
 #ifndef keys_header
 #define keys_header
 
+// These are included for kernel v5.4 support (Ubuntu LTS)
+// and can be safely removed after Ubuntu LTS updates (2025! :D)
+#ifndef KEY_NOTIFICATION_CENTER
+#define KEY_NOTIFICATION_CENTER 0x1bc
+#endif
+#ifndef KEY_PICKUP_PHONE
+#define KEY_PICKUP_PHONE        0x1bd	
+#endif
+#ifndef KEY_HANGUP_PHONE
+#define KEY_HANGUP_PHONE	    0x1be	
+#endif
+#ifndef KEY_FN_RIGHT_SHIFT
+#define KEY_FN_RIGHT_SHIFT	    0x1e5
+#endif
+
 /**
  * Checks if the key is a modifier key.
  */
