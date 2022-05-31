b8b1260e307e3cfa4c1770abb6d5fed6383c4cd0
donniebreve <donniebreve@protonmail.com>
2022-05-30 22:08:15 -0700

    updates to mapper code

--- src/mapper.c
+++ src/mapper.c
@@ -6,22 +6,25 @@
 #include "keys.h"
 #include "config.h"
 #include "emit.h"
+#include "mapper.h"
 
-// The state machine states
-enum states
-{
-    idle,
-    hyper,
-    delay,
-    map
-} state;
+// The state machine state
+enum states state = idle;
 
 // Flag if the hyper key has been emitted
 static int hyperEmitted;
 
 /**
+ * Converts input key to touch cursor key
+ * */
+static int convert(int code)
+{
+    return keymap[code];
+}
+
+/**
  * Checks if the key is the hyper key.
- */
+ * */
 static int isHyper(int code)
 {
     return code == hyperKey;
@@ -29,167 +32,176 @@ static int isHyper(int code)
 
 /**
  * Checks if the key has been mapped.
- */
+ * */
 static int isMapped(int code)
 {
     return keymap[code] != 0;
 }
 
 /**
- * Converts input key to touch cursor key
- */
-static int convert(int code)
+ * Checks if the key has been permanently mapped.
+ * */
+static int isRemapped(int code)
 {
-    return keymap[code];
+    return remap[code] != 0;
 }
 
 /**
  * Processes a key input event. Converts and emits events as necessary.
- */
+ * */
 void processKey(int type, int code, int value)
 {
     // printf("processKey: code=%i value=%i state=%i\n", code, value, state);
+    if (isRemapped(code))
+    {
+        code = remap[code];
+    }
     switch (state)
     {
         case idle: // 0
-            if (isHyper(code))
             {
-                if (isDown(value))
+                if (isHyper(code))
                 {
-                    state = hyper;
-                    hyperEmitted = 0;
-                    clearQueue();
+                    if (isDown(value))
+                    {
+                        state = hyper;
+                        hyperEmitted = 0;
+                        clearQueue();
+                    }
+                    else
+                    {
+                        emit(EV_KEY, code, value);
+                    }
                 }
                 else
                 {
                     emit(EV_KEY, code, value);
                 }
+                break;
             }
-            else
-            {
-                emit(EV_KEY, code, value);
-            }
-            break;
-
         case hyper: // 1
-            if (isHyper(code))
             {
-                if (!isDown(value))
+                if (isHyper(code))
                 {
-                    state = idle;
-                    if (!hyperEmitted)
+                    if (!isDown(value))
                     {
-                        emit(EV_KEY, hyperKey, 1);
+                        state = idle;
+                        if (!hyperEmitted)
+                        {
+                            emit(EV_KEY, hyperKey, 1);
+                        }
+                        emit(EV_KEY, hyperKey, 0);
                     }
-                    emit(EV_KEY, hyperKey, 0);
                 }
-            }
-            else if (isMapped(code))
-            {
-                if (isDown(value))
+                else if (isMapped(code))
                 {
-                    state = delay;
-                    enqueue(code);
+                    if (isDown(value))
+                    {
+                        state = delay;
+                        enqueue(code);
+                    }
+                    else
+                    {
+                        emit(EV_KEY, code, value);
+                    }
                 }
                 else
                 {
-                    emit(EV_KEY, code, value);
-                }
-            }
-            else
-            {
-                if (!isModifier(code) && isDown(value))
-                {
-                    if (!hyperEmitted)
+                    if (!isModifier(code) && isDown(value))
                     {
-                        emit(EV_KEY, hyperKey, 1);
-                        hyperEmitted = 1;
+                        if (!hyperEmitted)
+                        {
+                            emit(EV_KEY, hyperKey, 1);
+                            hyperEmitted = 1;
+                        }
+                        emit(EV_KEY, code, value);
                     }
-                    emit(EV_KEY, code, value);
+                    else
+                    {
+                        emit(EV_KEY, code, value);
+                    }
                 }
-                else
-                {
-                    emit(EV_KEY, code, value);
-                }
+                break;
             }
-            break;
-
         case delay: // 2
-            if (isHyper(code))
             {
-                if (!isDown(value))
+                if (isHyper(code))
                 {
-                    state = idle;
-                    if (!hyperEmitted)
+                    if (!isDown(value))
                     {
-                        emit(EV_KEY, hyperKey, 1);
+                        state = idle;
+                        if (!hyperEmitted)
+                        {
+                            emit(EV_KEY, hyperKey, 1);
+                        }
+                        int length = lengthOfQueue();
+                        for (int i = 0; i < length; i++)
+                        {
+                            emit(EV_KEY, dequeue(), 1);
+                        }
+                        emit(EV_KEY, hyperKey, 0);
                     }
-                    int length = lengthOfQueue();
-                    for (int i = 0; i < length; i++)
-                    {
-                        emit(EV_KEY, dequeue(), 1);
-                    }
-                    emit(EV_KEY, hyperKey, 0);
                 }
-            }
-            else if (isMapped(code))
-            {
-                state = map;
-                if (isDown(value))
+                else if (isMapped(code))
                 {
-                    if (lengthOfQueue() != 0)
+                    state = map;
+                    if (isDown(value))
                     {
-                        emit(EV_KEY, convert(peek()), 1);
+                        if (lengthOfQueue() != 0)
+                        {
+                            emit(EV_KEY, convert(peek()), 1);
+                        }
+                        enqueue(code);
+                        emit(EV_KEY, convert(code), value);
                     }
-                    enqueue(code);
-                    emit(EV_KEY, convert(code), value);
+                    else
+                    {
+                        int length = lengthOfQueue();
+                        for (int i = 0; i < length; i++)
+                        {
+                            emit(EV_KEY, convert(dequeue()), 1);
+                        }
+                        emit(EV_KEY, convert(code), value);
+                    }
                 }
                 else
                 {
-                    int length = lengthOfQueue();
-                    for (int i = 0; i < length; i++)
-                    {
-                        emit(EV_KEY, convert(dequeue()), 1);
-                    }
-                    emit(EV_KEY, convert(code), value);
+                    state = map;
+                    emit(EV_KEY, code, value);
                 }
+                break;
             }
-            else
-            {
-                state = map;
-                emit(EV_KEY, code, value);
-            }
-            break;
-
         case map: // 3
-            if (isHyper(code))
             {
-                if (!isDown(value))
+                if (isHyper(code))
                 {
-                    state = idle;
-                    int length = lengthOfQueue();
-                    for (int i = 0; i < length; i++)
+                    if (!isDown(value))
                     {
-                        emit(EV_KEY, convert(dequeue()), 0);
+                        state = idle;
+                        int length = lengthOfQueue();
+                        for (int i = 0; i < length; i++)
+                        {
+                            emit(EV_KEY, convert(dequeue()), 0);
+                        }
                     }
                 }
-            }
-            else if (isMapped(code))
-            {
-                if (isDown(value))
+                else if (isMapped(code))
                 {
-                    enqueue(code);
-                    emit(EV_KEY, convert(code), value);
+                    if (isDown(value))
+                    {
+                        enqueue(code);
+                        emit(EV_KEY, convert(code), value);
+                    }
+                    else
+                    {
+                        emit(EV_KEY, convert(code), value);
+                    }
                 }
                 else
                 {
-                    emit(EV_KEY, convert(code), value);
+                    emit(EV_KEY, code, value);
                 }
+                break;
             }
-            else
-            {
-                emit(EV_KEY, code, value);
-            }
-            break;
     }
 }
--- src/mapper.h
+++ src/mapper.h
@@ -1,8 +1,20 @@
-#ifndef touchcursor_header
-#define touchcursor_header
+#ifndef mapper_h
+#define mapper_h
 
-int isMapped(int code);
-int isModifier(int code);
+// The state machine states
+enum states
+{
+    idle,
+    hyper,
+    delay,
+    map
+};
+
+extern enum states state;
+
+/**
+ * Processes a key input event. Converts and emits events as necessary.
+ * */
 void processKey(int code, int type, int value);
 
 #endif
