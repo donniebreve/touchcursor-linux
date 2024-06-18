ec898fea33dccde6d673766a1c3e90c24f683450
auouymous <au@qzx.com>
2024-06-18 00:00:09 -0600

    Reduce emit latency by using a single write call.

--- src/emit.c
+++ src/emit.c
@@ -1,5 +1,6 @@
 #include <linux/input.h>
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
 #include "binding.h"
@@ -10,25 +11,29 @@
  * */
 void emit(int type, int code, int value)
 {
-    // printf("emit: code=%i value=%i\n", code, value);
-    struct input_event e;
-    e.time.tv_sec = 0;
-    e.time.tv_usec = 0;
-    // Set the virtual key code / value
-    e.type = type;
-    e.code = code;
-    e.value = value;
-    write(output_file_descriptor, &e, sizeof(e));
-
     if (type == EV_KEY)
     {
         // TODO: I don't like this here
         output_device_keystate[code] = value;
     }
 
+    // printf("emit: code=%i value=%i\n", code, value);
+    struct input_event e[2];
+    memset(e, 0, sizeof(e));
+
+    // Emit the virtual key code / value
+    // time.tv_sec = 0
+    // time.tv_usec = 0
+    e[0].type = type;
+    e[0].code = code;
+    e[0].value = value;
+
     // Emit a syn event
-    e.type = EV_SYN;
-    e.code = SYN_REPORT;
-    e.value = 0;
+    // time.tv_sec = 0
+    // time.tv_usec = 0
+    e[1].type = EV_SYN;
+    e[1].code = SYN_REPORT;
+    // value = 0
+
     write(output_file_descriptor, &e, sizeof(e));
 }
