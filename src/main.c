a653fa80eb7c0bac639f13c5c9bc90ba1021e929
Anako Jeannin-Mallet <49002220+anakojm@users.noreply.github.com>
2025-05-15 07:34:35 +0200

    fix: correct arg for pthread_create (#91)

--- src/main.c
+++ src/main.c
@@ -69,7 +69,7 @@ static int attach_signal_handlers()
 /**
  * Reads inotify watch events.
  * */
-static void* read_watch_events()
+static void* read_watch_events(void *)
 {
     struct inotify_event event;
     while (1)
